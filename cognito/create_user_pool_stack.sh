8dd3b91268009c2eb4a87f3f6d7a37579aaea19b
Rafael Costa <rafaelcmd@gmail.com>
2024-05-19 02:39:24 -0300

    cognito user pool stack returning user pool client id

---
+++ cognito/create_user_pool_stack.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create the Cognito User Pool stack
+aws cloudformation create-stack \
+    --stack-name cognito-user-pool \
+    --template-body file://create_user_pool.yaml \
+    --capabilities CAPABILITY_NAMED_IAM
+
+# Wait for the stack creation to complete
+aws cloudformation wait stack-create-complete --stack-name cognito-user-pool
+
+# Get the User Pool Client ID from the stack output
+USER_POOL_CLIENT_ID=$(aws cloudformation describe-stacks \
+    --stack-name cognito-user-pool \
+    --output text \
+    --query 'Stacks[0].Outputs[?OutputKey==`UserPoolClientId`].OutputValue')
+
+# Export the User Pool Client ID as an environment variable
+export USER_POOL_CLIENT_ID
+
+# Print the environment variable to verify it's set
+echo "USER_POOL_CLIENT_ID: $USER_POOL_CLIENT_ID"
